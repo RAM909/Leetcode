b835a9e2abcaf21696e5a07400a5f44d1e74eed5
Ramesh yadav <111211497+RAM909@users.noreply.github.com>
2024-08-17 22:56:46 +0530

    Time: 1343 ms (34.51%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select y.product_name,x.year,x.price as "price"
+select y.product_name as "product_name",x.year,x.price as "price"
 from sales x inner join product y on x.product_id = y.product_id
