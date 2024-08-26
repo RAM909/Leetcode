dd7d3e1e308b13459767cf72db2a6fdc688b1718
Ramesh yadav <111211497+RAM909@users.noreply.github.com>
2024-08-26 23:21:18 +0530

    Time: 236 ms (95.15%), Space: 0B (100.00%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select name as "name",population as "population",area as "area" from world where area >= 3000000 or population >= 25000000
+select name as "Name",population as "Population",area as "Area" from world where area >= 3000000 or population >= 25000000
